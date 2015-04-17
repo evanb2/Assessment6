3224fd87540d509f400a8c4756417142f3017791
Evan Butler <evanb2@mac.com>
2015-04-17 10:18:16 -0700

    write first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-var (name) = function(input) {
-
+var Pizza = {
+    priceCalc: function() {
+      return 6 + (this.topping * 1)
+               + (this.size * 2)
+               + (this.delivery * 3)
+    }
 };
 
 
 $(document).ready(function(event) {
+  $("form#pizza-cost").submit(function(event) {
+    event.preventDefault();
 
+    var topping = perseInt($("#topping").val());
+    var size = parseInt($("#size").val());
+    var delivery = parseInt($("#delivery").val());
+
+    var newPizza = Object.create(Pizza);
+
+    newPizza.topping = topping;
+    newPizza.size = size;
+    newPizza.delivery = delivery;
+
+    var pizzaPrice = newPizza.priceCalc();
+
+    $(".result").text("$ " + pizzaPrice + ".00");
+  });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,14 @@
-describe("titleCase", function() {
-    it("capitalizes the first letter of a word", function() {
-        expect(titleCase("cat")).to.equal("Cat");
+describe("Pizza", function() {
+  describe("priceCalc()", function() {
+    it("returns the pizza price, base values in each category", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = "Cheese";
+        testPizza.size = "Personal Pan";
+        testPizza.delivery = "Take out";
+        expect(testPizza.priceCalc()).to.eql(6);
     });
+  });
 });
+
+
+//Formula = 6 + (this.topping(0 1 2) * 1) + (this.size(0 1 2 ) * 2) + (this.delivery(0 1 5) * 3)
