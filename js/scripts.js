acc781b38287c0eb0756154c15cf90e24a21a7a8
Evan Butler <evanb2@mac.com>
2015-04-17 10:33:41 -0700

    pass all specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var Pizza = {
     priceCalc: function() {
       return 6 + (this.topping * 1)
-               + (this.size * 2)
+               + (this.pizzaSize * 2)
                + (this.delivery * 3)
     }
 };
@@ -11,14 +11,14 @@ $(document).ready(function(event) {
   $("form#pizza-cost").submit(function(event) {
     event.preventDefault();
 
-    var topping = perseInt($("#topping").val());
-    var size = parseInt($("#size").val());
+    var topping = parseInt($("#topping").val());
+    var pizzaSize = parseInt($("#size").val());
     var delivery = parseInt($("#delivery").val());
 
     var newPizza = Object.create(Pizza);
 
     newPizza.topping = topping;
-    newPizza.size = size;
+    newPizza.pizzaSize = pizzaSize;
     newPizza.delivery = delivery;
 
     var pizzaPrice = newPizza.priceCalc();
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,71 @@
 describe("Pizza", function() {
   describe("priceCalc()", function() {
-    it("returns the pizza price, base values in each category", function() {
+    it("returns the pizza price: base values in each category", function() {
         var testPizza = Object.create(Pizza);
-        testPizza.topping = "Cheese";
-        testPizza.size = "Personal Pan";
-        testPizza.delivery = "Take out";
+        testPizza.topping = 0;
+        testPizza.pizzaSize = 0;
+        testPizza.delivery = 0;
         expect(testPizza.priceCalc()).to.eql(6);
+    });
+  });
+
+  describe("priceCalc()", function() {
+    it("returns the pizza price: topping = Pepperoni, size = Personal Pan, delivery = Take out", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = 1;
+        testPizza.pizzaSize = 0;
+        testPizza.delivery = 0;
+        expect(testPizza.priceCalc()).to.eql(7);
+    });
+  });
+
+  describe("priceCalc()", function() {
+    it("returns the pizza price: topping = Sausage, size = Personal Pan, delivery = Take out", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = 2;
+        testPizza.pizzaSize = 0;
+        testPizza.delivery = 0;
+        expect(testPizza.priceCalc()).to.eql(8);
+    });
+  });
+
+  describe("priceCalc()", function() {
+    it("returns the pizza price: topping = Sausage, size = 12, delivery = Take out", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = 2;
+        testPizza.pizzaSize = 1;
+        testPizza.delivery = 0;
+        expect(testPizza.priceCalc()).to.eql(10);
+    });
+  });
+
+  describe("priceCalc()", function() {
+    it("returns the pizza price: topping = Sausage, size = 16, delivery = Take out", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = 2;
+        testPizza.pizzaSize = 2;
+        testPizza.delivery = 0;
+        expect(testPizza.priceCalc()).to.eql(12);
+    });
+  });
+
+  describe("priceCalc()", function() {
+    it("returns the pizza price: topping = Sausage, size = 16, delivery = Delivery", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = 2;
+        testPizza.pizzaSize = 2;
+        testPizza.delivery = 1;
+        expect(testPizza.priceCalc()).to.eql(15);
+    });
+  });
+
+  describe("priceCalc()", function() {
+    it("returns the pizza price: topping = Sausage, size = 16, delivery = Ship", function() {
+        var testPizza = Object.create(Pizza);
+        testPizza.topping = 2;
+        testPizza.pizzaSize = 2;
+        testPizza.delivery = 5;
+        expect(testPizza.priceCalc()).to.eql(27);
     });
   });
 });
